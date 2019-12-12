a73bfaeeb3f1a274e66b09ee707c9a0a7b4c5d54
Gabriela Hołowacz <57449657+gholowacz@users.noreply.github.com>
2019-12-12 20:53:39 +0100

    Add files via upload
    Update on the Bulls and Cows Game

---
+++ BullsCows.cpp
@@ -0,0 +1,69 @@
+﻿// BullsCows.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+void PlayGame();
+string GetGuess(); 
+
+
+// the entry point for our application
+
+int main()
+{
+	PrintIntro();
+	
+	PlayGame();
+
+	return 0; // exit the application 
+}
+
+
+
+// introduce the game
+void PrintIntro() 
+{
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows\n";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter word I'm thinking about??\n";
+	cout << endl;
+	return;
+}
+
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		string guess = GetGuess();
+		cout << "Your guess was: " << guess << endl;
+		cout << endl;
+	}
+}
+
+
+string GetGuess() 
+{
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	return Guess; 
+}
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
